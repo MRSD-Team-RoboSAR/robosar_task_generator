a34192543937920ffd288d63d36d7831501120e0
JSongLaptop <you@example.com>
2022-02-28 18:47:27 -0500

    Modified pgm_reader for localization_map_vicon

--- scripts/pgm_reader.py
+++ scripts/pgm_reader.py
@@ -3,18 +3,18 @@ import numpy as np
 def read_pgm():
     """Return a raster of integers from a PGM as a list of lists."""
     # Open file
-    pgmf = open('../maps/willow-full.pgm', 'rb')
+    pgmf = open('../maps/localization_map_vicon.pgm', 'rb')
     # Discard first line; P5
-    (pgmf.readline())
+    print(pgmf.readline())
     # Discard second line; resolution
-    (pgmf.readline())
+    print(pgmf.readline())
     # Discard third line; dimensions
-    (pgmf.readline())
+    print(pgmf.readline())
     # Discard fourth line; depth
-    (pgmf.readline())
+    print(pgmf.readline())
     # Hardcode values for map for now
-    width = 584
-    height = 526
+    width = 168
+    height = 101
     depth = 255
     # Read in map from file
     map = np.zeros((width,height))
@@ -51,12 +51,19 @@ if __name__ == "__main__":
     end = time.time()
     elapsed_time = end-start
     print("Elapsed time: ", elapsed_time, "s")
+    # Hardcode to remove bad waypoints
+    to_remove_x = waypoints[:,0] < 55
+    to_remove_y = waypoints[:,1] < 26
+    to_remove = np.logical_or(to_remove_x, to_remove_y)
+    to_keep = np.logical_not(to_remove)
+    waypoints = waypoints[to_keep]
+
     taskgen.visualize_circles(waypoints[:,0:2], waypoints[:,2])
     plt.xlim([0, test_map.shape[0]])
     plt.ylim([0, test_map.shape[1]])
     plt.show()
     # Export waypoints
-    np.save("../outputs/willow-full",waypoints[:,0:2])
+    np.save("../outputs/localization_map_vicon",waypoints[:,0:2])
     # Fewer waypoints
     otsu_threshold = skimage.filters.threshold_otsu(waypoints[:,2])
     otsu_mask = waypoints[:,2] >= otsu_threshold
@@ -66,6 +73,6 @@ if __name__ == "__main__":
     plt.ylim([0, test_map.shape[1]])
     plt.show()
     # Export waypoints_lean
-    np.save("../outputs/willow-full_lean",waypoints_lean[:,0:2])
+    np.save("../outputs/localization_map_vicon_lean",waypoints_lean[:,0:2])
 
 
