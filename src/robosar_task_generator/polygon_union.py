e3a5b658e3402d48c4b904417669977367e6884d
JSongLaptop <you@example.com>
2022-03-29 20:29:04 -0400

    Integrated pyclipper with polygon_generator

--- src/robosar_task_generator/polygon_union.py
+++ src/robosar_task_generator/polygon_union.py
@@ -1,37 +1,73 @@
-""" Based on following paper: Free-space Polygon Creation based on Occupancy Grid Maps for Trajectory Optimization Methods"""
-
 import numpy as np
 from matplotlib import pyplot as plt
 import pyclipper
-import polygon_generator as pg
+import polygon_generator as polygen
+from scipy import ndimage
 
 if __name__ == "__main__":
-    subj = (
-    ((180, 200), (260, 200), (260, 150), (180, 150)),
-    ((215, 160), (230, 190), (200, 190))
-    )
-    clip = ((190, 210), (240, 210), (240, 130), (190, 130))
-    
-    poly1 = np.matrix(subj[0])
-    poly2 = np.matrix(subj[1])
-    poly3 = np.matrix(clip)
-    poly_list = []
-    poly_list.append(poly1)
-    poly_list.append(poly2)
-    poly_list.append(poly3)
-    pg.plot_polygon_list(poly_list)
+    """ Get polygons """
+    # Generate map; map uses (x,y) not (row,col)
+    test_map = np.zeros((300, 400))
+    # Boundaries
+    test_map[[0,-1],:] = 1
+    test_map[:,[0,-1]] = 1
+    # Horizontal walls
+    test_map[75:125, [100,200,300]] = 1
+    test_map[175:, [100,200,300]] = 1
+    # Vertical walls
+    test_map[[125,175], 75:125] = 1
+    test_map[[125,175], 175:225] = 1
+    test_map[[125,175], 275:325] = 1
+    # Thicken
+    test_map = ndimage.binary_dilation(test_map, iterations=4)
+    test_map = test_map.astype('int')
+
+    polygen.plot_map(test_map, 'k')
     plt.show()
 
-    pc = pyclipper.Pyclipper()
-    pc.AddPath(clip, pyclipper.PT_CLIP, True)
-    pc.AddPaths(subj, pyclipper.PT_SUBJECT, True)
+    # Arguments
+    test_pnts = np.array([
+        [50, 50],
+        [150, 50],
+        [250, 50],
+        [50, 150],
+        [150, 150],
+        [250, 150],
+        [50, 250],
+        [150, 250],
+        [250, 250],
+        [50, 350],
+        [150, 350],
+        [250, 350]
+    ])
+    test_angle = 0
+    num_vert = 10
+    radius = 10
+    max_range = 100
+    threshold = 0.5
 
-    solution = pc.Execute(pyclipper.CT_INTERSECTION, pyclipper.PFT_EVENODD, pyclipper.PFT_EVENODD)
-    sol1 = np.matrix(solution[0])
-    sol2 = np.matrix(solution[1])
+    polys = polygen.multi_polygon_generation(test_pnts, test_angle, test_map, num_vert, radius, max_range, threshold)
+    
+    polygen.plot_map(test_map, 'k')
+    plt.scatter(test_pnts[:,0], test_pnts[:,1])
+    polygen.plot_polygon_list(polys)
+    plt.show()
+
+    """ Union """
+    # Set up subject list
+    subj_list = []
+    for poly in polys:
+        subj_list.append(poly.tolist())
+    
+    # Compute union
+    pc = pyclipper.Pyclipper()
+    pc.AddPaths(subj_list, pyclipper.PT_SUBJECT, True)
+    solution = pc.Execute(pyclipper.CT_UNION, pyclipper.PFT_NONZERO)
+
     poly_list2 = []
-    poly_list2.append(sol1)
-    poly_list2.append(sol2)
-    pg.plot_polygon_list(poly_list2)
+    for poly in solution:
+        poly_list2.append(np.matrix(poly))
+    polygen.plot_map(test_map, 'k')
+    polygen.plot_polygon_list(poly_list2)
     plt.show()
     print("Done")
