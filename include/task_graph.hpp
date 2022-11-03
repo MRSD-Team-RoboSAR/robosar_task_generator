28e8cc71d1337d7a1febb73a72b3a0c57b4f800b
Neel <ipatil@andrew.cmu.edu>
2022-11-03 17:22:16 -0400

    Finished constructing the graph and created a new thread

--- include/task_graph.hpp
+++ include/task_graph.hpp
@@ -7,11 +7,13 @@
 #include <string>
 #include <queue>
 #include <vector>
+#include <thread>
 
 // ROS
 #include <ros/ros.h>
 #include "robosar_messages/pose_graph.h"
 #include <geometry_msgs/Pose.h>
+#include <visualization_msgs/MarkerArray.h>
 
 
 class TaskGraph {
@@ -22,21 +24,25 @@ public:
     class TaskVertex {
     public:
         TaskVertex() : pose_(), neighbors_() {}
-        TaskVertex(int id, geometry_msgs::Pose pose) : id_(id), pose_(pose), neighbors_() {}
+        TaskVertex(int id, geometry_msgs::Pose pose) : id_(id), pose_(pose), neighbors_(), info_updated_(false) {}
 
         int id_;
         geometry_msgs::Pose pose_;
         std::vector<int> neighbors_;
+        bool info_updated_;
     };
 
 private:
-    void incomingGraph(const robosar_messages::pose_graphConstPtr& new_graph);
+    void incomingGraph(const visualization_msgs::MarkerArrayConstPtr& new_graph);
+    void coverageTaskGenerator();
 
     std::map<int,int> id_to_index_;
     std::vector<TaskVertex> V_;
     ros::NodeHandle nh_;
     ros::Subscriber graph_sub_;
     std::mutex mtx;
+    bool new_data_rcvd_;
+    std::thread node_thread_;
 };
 
 #endif //TASK_GRAPH_H
--- src/robosar_task_generator/task_graph.cpp
+++ src/robosar_task_generator/task_graph.cpp
@@ -2,14 +2,65 @@
 
 #include "task_graph.hpp"
 
-TaskGraph::TaskGraph() : nh_("") {
+
+TaskGraph::TaskGraph() : nh_(""), new_data_rcvd_(false) {
     // TODO
 
-    graph_sub_ = nh_.subscribe("pose_graph", 1, &TaskGraph::incomingGraph, this);
+    graph_sub_ = nh_.subscribe("/slam_toolbox/karto_graph_visualization", 1, &TaskGraph::incomingGraph, this);
+
+    ROS_INFO("Task Graphing!");
+    node_thread_ = std::thread(&TaskGraph::coverageTaskGenerator, this);
 }
 
-void TaskGraph::incomingGraph(const robosar_messages::pose_graphConstPtr& new_graph)
+inline bool AreSame(double a, double b)
+{
+    return fabs(a - b) < std::numeric_limits<double>::epsilon();
+}
+
+
+void TaskGraph::incomingGraph(const visualization_msgs::MarkerArrayConstPtr& new_graph)
 {
   std::lock_guard<std::mutex> guard(mtx);
 
+  // Go through all the markers in the array and add them to the graph
+  for (auto marker : new_graph->markers)
+  {
+    // Check if the marker is a node
+    if (marker.type == visualization_msgs::Marker::SPHERE)
+    {
+      // Check if the node already exists
+      if (id_to_index_.find(marker.id) == id_to_index_.end())
+      {
+        // Add the node to the graph
+        TaskVertex new_vertex(marker.id, marker.pose);
+        V_.push_back(new_vertex);
+        id_to_index_[marker.id] = V_.size() - 1;
+        new_data_rcvd_ = true;
+      }
+      else {
+        // Check if there is a change in the pose
+        TaskVertex graph_vertex = V_[id_to_index_[marker.id]];
+        if(!AreSame(graph_vertex.pose_.position.x, marker.pose.position.x) || 
+            !AreSame(graph_vertex.pose_.position.y, marker.pose.position.y)) {
+          // Update the pose
+          graph_vertex.pose_ = marker.pose;
+          graph_vertex.info_updated_ = true;
+          V_[id_to_index_[marker.id]] = graph_vertex;
+          new_data_rcvd_ = true;
+        }
+      }
+    }
+    // Check if the marker is an edge
+    else if (marker.type == visualization_msgs::Marker::ARROW)
+    {
+      ROS_WARN("Edge marker found");
+    }
+  }
+
+  ROS_WARN("New size of graph: %ld", V_.size());
+
+}
+
+void TaskGraph::coverageTaskGenerator() {
+  
 }
