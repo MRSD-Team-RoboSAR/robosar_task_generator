e1fa27b842aa56e4cb2833d3a7ca198329dd261b
Neel <ipatil@andrew.cmu.edu>
2022-11-04 20:49:34 -0400

    Added functions to calculate information gain and check if it is a coverage node

--- include/task_graph.hpp
+++ include/task_graph.hpp
@@ -15,36 +15,57 @@
 #include "robosar_messages/pose_graph.h"
 #include <geometry_msgs/Pose.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <nav_msgs/OccupancyGrid.h>
 
 
 class TaskGraph {
 
 public:
     TaskGraph();
+    ~TaskGraph();
 
     class TaskVertex {
     public:
         TaskVertex() : pose_(), neighbors_() {}
-        TaskVertex(int id, geometry_msgs::Pose pose) : id_(id), pose_(pose), neighbors_(), info_updated_(false) {}
+        TaskVertex(int id, geometry_msgs::Pose pose, bool is_coverage_node) 
+                : id_(id), pose_(pose), neighbors_(), info_updated_(true), is_coverage_node_(is_coverage_node_) {}
 
+        float get_info_gain_radius() { return info_gain_radius_; };
         int id_;
         geometry_msgs::Pose pose_;
         std::vector<int> neighbors_;
         bool info_updated_;
+        float info_gain_radius_;
+        bool is_coverage_node_;
+
     };
 
 private:
     void incomingGraph(const visualization_msgs::MarkerArrayConstPtr& new_graph);
     void coverageTaskGenerator();
+    void initMarkers(void);
+    void visualizeMarkers(void);
+    float informationGain(std::pair<float, float> &x);
+    int gridValue(std::pair<float, float> &Xp);
+    void mapCallBack(const nav_msgs::OccupancyGrid::ConstPtr &msg);
+    bool isValidCoveragePoint(std::pair<float, float> x_new, float info_radius, int id);
+
+    visualization_msgs::Marker marker_points, marker_line, marker_coverage_area, marker_points_coverage;
+    visualization_msgs::MarkerArray marker_coverage_area_array;
+    ros::NodeHandle nh_;
+    ros::Subscriber graph_sub_;
+    ros::Subscriber map_sub_;
+    ros::Publisher marker_coverage_area_pub_;
+    ros::Publisher marker_pub_;
+    nav_msgs::OccupancyGrid mapData_;
 
     std::map<int,int> id_to_index_;
     std::vector<TaskVertex> V_;
-    ros::NodeHandle nh_;
-    ros::Subscriber graph_sub_;
     std::mutex mtx;
     bool new_data_rcvd_;
     std::thread node_thread_;
     std::condition_variable cv_;
+    std::string frame_id_;
 };
 
 #endif //TASK_GRAPH_H
--- src/robosar_task_generator/task_graph.cpp
+++ src/robosar_task_generator/task_graph.cpp
@@ -3,13 +3,35 @@
 #include "task_graph.hpp"
 
 
-TaskGraph::TaskGraph() : nh_(""), new_data_rcvd_(false) {
+// Coverage planner parameters
+#define COV_MIN_INFO_GAIN_RADIUS_M 0.5
+#define COV_PERCENT_COVERAGE_OVERLAP 100 // TODO
+#define COV_MAX_INFO_GAIN_RADIUS_M 5.0 // Should reflect sensor model
+std::vector<std::vector<int>> bfs_prop_model = {{-1 , 0}, {0 , -1}, {0 , 1}, {1 , 0}};
+
+TaskGraph::TaskGraph() : nh_(""), new_data_rcvd_(false), frame_id_("map") {
     // TODO
 
     graph_sub_ = nh_.subscribe("/slam_toolbox/karto_graph_visualization", 1, &TaskGraph::incomingGraph, this);
+    map_sub_ = nh_.subscribe("/map", 100, &TaskGraph::mapCallBack, this);
+    marker_coverage_area_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("/task_graph/coverage_area", 10);
+    marker_pub_ = nh_.advertise<visualization_msgs::Marker>("/task_graph/points", 10);
 
+    initMarkers();
     ROS_INFO("Task Graphing!");
     node_thread_ = std::thread(&TaskGraph::coverageTaskGenerator, this);
+}
+
+// Subscribers callback functions---------------------------------------
+void TaskGraph::mapCallBack(const nav_msgs::OccupancyGrid::ConstPtr &msg)
+{
+    mapData_ = *msg;
+}
+
+TaskGraph::~TaskGraph() {
+    new_data_rcvd_ = true;
+    cv_.notify_one();
+    node_thread_.join();
 }
 
 inline bool AreSame(double a, double b)
@@ -17,6 +39,59 @@ inline bool AreSame(double a, double b)
     return fabs(a - b) < std::numeric_limits<double>::epsilon();
 }
 
+
+void TaskGraph::initMarkers()
+{
+    // visualizations  points and lines..
+    marker_points.header.frame_id = frame_id_;
+    marker_line.header.frame_id = frame_id_;
+    marker_points.header.stamp = ros::Time(0);
+    marker_line.header.stamp = ros::Time(0);
+
+    marker_points.ns = marker_line.ns = "markers";
+    marker_points.id = 0;
+    marker_line.id = 1;
+
+    marker_points.type = marker_points.POINTS;
+    marker_line.type = marker_line.LINE_LIST;
+
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+    marker_points.action = marker_points.ADD;
+    marker_line.action = marker_line.ADD;
+    marker_points.pose.orientation.w = 1.0;
+    marker_line.pose.orientation.w = 1.0;
+    marker_line.scale.x = 0.03;
+    marker_line.scale.y = 0.03;
+    marker_points.scale.x = 0.3;
+    marker_points.scale.y = 0.3;
+
+    marker_line.color.r = 9.0 / 255.0;
+    marker_line.color.g = 91.0 / 255.0;
+    marker_line.color.b = 236.0 / 255.0;
+    marker_points.color.r = 255.0 / 255.0;
+    marker_points.color.g = 0.0 / 255.0;
+    marker_points.color.b = 0.0 / 255.0;
+    marker_points.color.a = 1.0;
+    marker_line.color.a = 1.0;
+    marker_points.lifetime = ros::Duration();
+    marker_line.lifetime = ros::Duration();
+
+    // marker for coverage points
+    marker_points_coverage = marker_points;
+    marker_points_coverage.color.r = 0.0;
+    marker_points_coverage.color.g = 255.0 / 255.0;
+    marker_points_coverage.color.b = 255.0 / 255.0;
+
+    // marker for coverage area
+    marker_coverage_area = marker_points;
+    marker_coverage_area.type = marker_coverage_area.CYLINDER;
+    marker_coverage_area.color.r = 0.0;
+    marker_coverage_area.color.g = 0.0;
+    marker_coverage_area.color.b = 255.0 / 255.0;
+    marker_coverage_area.color.a = 0.2;
+    marker_coverage_area.scale.z = 0.01;
+
+}
 
 void TaskGraph::incomingGraph(const visualization_msgs::MarkerArrayConstPtr& new_graph)
 {
@@ -33,7 +108,7 @@ void TaskGraph::incomingGraph(const visualization_msgs::MarkerArrayConstPtr& new
       if (id_to_index_.find(marker.id) == id_to_index_.end())
       {
         // Add the node to the graph
-        TaskVertex new_vertex(marker.id, marker.pose);
+        TaskVertex new_vertex(marker.id, marker.pose, false);
         V_.push_back(new_vertex);
         id_to_index_[marker.id] = V_.size() - 1;
         local_graph_update = true;
@@ -58,9 +133,9 @@ void TaskGraph::incomingGraph(const visualization_msgs::MarkerArrayConstPtr& new
     }
   }
 
-  ROS_WARN("New size of graph: %ld", V_.size());
   // Inform main thread
   if(local_graph_update) {
+    ROS_WARN("New size of graph: %ld", V_.size());
     new_data_rcvd_ = true;
     cv_.notify_one();
   }
@@ -75,9 +150,218 @@ void TaskGraph::coverageTaskGenerator() {
 
     ROS_WARN("Running coverage task generator");
 
-    
+    // Iterate through all the vertices and update information gain
+    for(auto& vertex : V_) {
+      if(vertex.info_updated_) {
+        ROS_WARN("Vertex %d has been updated", vertex.id_);
+
+        std::pair<float,float> vertex_pos = std::make_pair(vertex.pose_.position.x, vertex.pose_.position.y);
+        vertex.info_gain_radius_ = informationGain(vertex_pos);
+        ROS_INFO("%f ",vertex.info_gain_radius_);
+      }
+    }
+
+    // Iterate again and update coverage points
+    for(auto& vertex : V_) {
+      if(vertex.info_updated_) {
+
+        // Check if valid coverage point
+        std::pair<float,float> vertex_pos = std::make_pair(vertex.pose_.position.x, vertex.pose_.position.y);
+        if(isValidCoveragePoint(vertex_pos, vertex.get_info_gain_radius() , vertex.id_)) {
+          vertex.is_coverage_node_ = true;
+          std::cout<<"Coverage!"<<std::endl;
+        } else {
+          vertex.is_coverage_node_ = false;
+        }
+
+        vertex.info_updated_ = false;
+      }
+    }
+
+    visualizeMarkers();
 
     new_data_rcvd_ = false;
     lk.unlock();
   }
 }
+
+float Norm(float x1, float y1, float x2, float y2)
+{
+    return pow((pow((x2 - x1), 2) + pow((y2 - y1), 2)), 0.5);
+}
+
+float TaskGraph::informationGain(std::pair<float, float> &x) {
+  
+  // Local variables
+  float info_gain_radius_m = 0.0;
+  bool end_of_bfs = false;
+  // BFS around this point to find distance to nearest obstacle
+
+  // Local variables
+  std::queue<std::pair<int,int>> queue;
+  std::set<int> visited;
+
+  //Convert to pixel and add it to queue
+  std::pair<int,int> start_ind = std::make_pair((int)((x.first-mapData_.info.origin.position.x)/mapData_.info.resolution), 
+                                                (int)((x.second-mapData_.info.origin.position.y)/mapData_.info.resolution));
+  queue.push(start_ind);
+  
+  while(!queue.empty()&& !end_of_bfs) {
+    std::pair<int,int> current = queue.front();
+    queue.pop();
+
+    //if already visited, continue to next iteration
+    if(visited.find((current.first + current.second*mapData_.info.width)) != visited.end()) 
+      continue;
+    // insert into visited
+    else
+      visited.insert(current.first + current.second*mapData_.info.width);
+
+    // Check if reached end of BFS radius
+    //convert to world coordinates
+    std::pair<float,float> current_position = std::make_pair(mapData_.info.origin.position.x + mapData_.info.resolution*(float)(current.first), 
+                                  mapData_.info.origin.position.y + mapData_.info.resolution*(float)(current.second));
+
+    float dist = Norm(current_position.first,current_position.second, 
+                        x.first,x.second);
+    if(dist > info_gain_radius_m) {
+      info_gain_radius_m = dist;
+    }
+    if(dist > COV_MAX_INFO_GAIN_RADIUS_M) {
+      //ROS_INFO("Outside radius. Exiting obstacle search");
+      end_of_bfs = true;
+    }
+
+    // Check if obstacle
+    if(gridValue(current_position) == 100 || gridValue(current_position) == -1) {
+      end_of_bfs = true;
+    }
+
+    //expand node
+    for(const auto& dir : bfs_prop_model) {
+      std::pair<int,int> neighbour_ind = std::make_pair(current.first + dir[0], current.second + dir[1]);
+      //Boundary checks
+      if (neighbour_ind.first >= 0 && neighbour_ind.first < mapData_.info.width && 
+          neighbour_ind.second >= 0 && neighbour_ind.second < mapData_.info.height ) {
+            queue.push(neighbour_ind);
+      }
+      else
+        end_of_bfs = true;
+    }
+  }
+
+   return info_gain_radius_m;
+ }
+
+
+bool TaskGraph::isValidCoveragePoint(std::pair<float, float> x_new, float info_radius, int id ) {    
+    
+    // Local variables
+    bool valid_node = true;
+    std::vector<int> to_remove;
+
+    // parameter check
+    if(info_radius < COV_MIN_INFO_GAIN_RADIUS_M) {
+        return false;
+    }
+
+    int ind=0;
+    for (auto j = V_.begin(); j != V_.end(); j++) {
+
+        // Dont compare with yourself
+        if(j->id_ == id)
+            continue;
+
+        // eliminate node which is more than 2*info_radius away from x_new
+        float max_info_radius = std::max(info_radius, j->get_info_gain_radius());
+        if(fabs(x_new.first-j->pose_.position.x) > 2*max_info_radius 
+            || fabs(x_new.second-j->pose_.position.y) > 2*max_info_radius) {
+            continue;
+        }
+
+        // TODO should we check if it is a coverage node?
+        float inter_node_dist = Norm(j->pose_.position.x, j->pose_.position.y,
+                                        x_new.first, x_new.second); 
+        // If there is an overlap (either of the centers is inside the other circle)
+        // keep the node with the largest radius
+        // TODO can control per cent overlap
+        if((info_radius > inter_node_dist || j->get_info_gain_radius() > inter_node_dist)) {
+            if(info_radius < j->get_info_gain_radius()) {
+
+                std::cout<<j->get_info_gain_radius()<<" "<<info_radius<<std::endl;
+                valid_node = false;
+                break;
+            }
+            else {
+                to_remove.push_back(ind);
+            }
+        }
+
+        ind++;
+    }
+
+    if(valid_node) {
+        for (int id : to_remove) {
+            V_[ind].is_coverage_node_ = false;
+        }
+    }
+    return valid_node;
+}
+
+ // gridValue function
+int TaskGraph::gridValue(std::pair<float, float> &Xp)
+{
+    float resolution = mapData_.info.resolution;
+    float Xstartx = mapData_.info.origin.position.x;
+    float Xstarty = mapData_.info.origin.position.y;
+
+    float width = mapData_.info.width;
+    std::vector<signed char> Data = mapData_.data;
+
+    // returns grid value at "Xp" location
+    // map data:  100 occupied      -1 unknown       0 free
+    float indx = (floor((Xp.second - Xstarty) / resolution) * width) + (floor((Xp.first - Xstartx) / resolution));
+    int out;
+    out = Data[int(indx)];
+    return out;
+}
+
+ void TaskGraph::visualizeMarkers(void) {
+
+    // visualization
+    marker_points_coverage.points.clear();
+    marker_coverage_area_array.markers.clear();
+
+    // Clear old Markers
+    visualization_msgs::Marker delete_all;
+    delete_all.action = visualization_msgs::Marker::DELETEALL;
+    delete_all.id = 0;
+    marker_coverage_area_array.markers.push_back(delete_all);
+    marker_coverage_area_pub_.publish(marker_coverage_area_array);
+    marker_coverage_area_array.markers.clear();
+
+    for (auto j = V_.begin(); j != V_.end(); j++)
+    {
+        geometry_msgs::Point p;
+        p.x = j->pose_.position.x;
+        p.y = j->pose_.position.y;
+        p.z = 0.0;
+
+        if(j->is_coverage_node_) {
+            
+            // Create coverage point marker
+            marker_points_coverage.points.push_back(p);
+
+            // Create area marker              
+            marker_coverage_area.id += 1;
+            marker_coverage_area.pose.position.x = p.x;
+            marker_coverage_area.pose.position.y = p.y;
+            marker_coverage_area.scale.x = 2 * j->get_info_gain_radius();
+            marker_coverage_area.scale.y = 2 * j->get_info_gain_radius();
+            marker_coverage_area_array.markers.push_back(marker_coverage_area);
+        }
+    }
+
+    marker_pub_.publish(marker_points_coverage);
+    marker_coverage_area_pub_.publish(marker_coverage_area_array);
+ }
